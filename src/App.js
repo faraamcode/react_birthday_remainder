54c7a3d8e0f8abeef76395fb8d508b24094abef0
faraamcode <abdulrasaqalagbede@gmail.com>
2020-12-31 12:06:29 +0000

    birthday are genrated base on data on data.js we can connect it with any desired api and filter it more

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [user, setUser] = useState(data);
+ 
+  return (
+    <>
+    <section className="container">
+      <h3>{user.length} birthday today</h3>
+      <List user = {user}/>
+      <button className="btn" onClick={()=>{
+        setUser([])
+      }}>Clear birthday</button>
+    </section>
+    </>
+  );
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const List = () => {
+const List = ({user}) => {
   return (
     <>
-      <h2>list component</h2>
+      {user.map((birthday_user)=>{
+        const {id, name, age, image} = birthday_user;
+        return (
+          <article key={id} className="person">
+           <img src={image} alt={name}/>
+           <div>
+            <h4>{name}</h4>
+            <p>{age} years</p>
+           </div>
+          </article>
+        );
+      })}
     </>
   );
 };
